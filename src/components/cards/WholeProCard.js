73d7767ba4958433c04d2f693277c2508390a5ba
yxie80 <michael.monash@gmail.com>
2019-11-10 15:19:42 +1100

    second edit

--- src/components/cards/WholeProCard.js
+++ src/components/cards/WholeProCard.js
@@ -4,8 +4,12 @@ import WholePro from '../WholePro';
 
 class WholeProCard extends React.Component {
     render() {
+        const styleObj = {
+            fontSize: 20
+        }
         return (
             <Card style={{ width: 300 }}>
+                <p style={styleObj}>Progress for all lands</p>
                 <WholePro/>
             </Card>
         );
